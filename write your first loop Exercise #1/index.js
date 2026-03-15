bd35424cb7f53228b16308546a0b43021a5ba7e0
Namtii <hit4@o2.pl>
2026-03-15 21:28:20 +0100

    scrimba rite your first loop Exercise #1
    https://scrimba.com/learn-javascript-c0v/~02b/s0lj7pg34o/head

---
+++ write your first loop Exercise #1/index.js
@@ -0,0 +1,12 @@
+// for (let i = 0; i < 6; i += 1) {
+//     console.log(i)
+// }
+
+// Create a for loop that counts from 10 to 100 in steps of 10
+// Use console.log to log out the numbers
+
+for (let ii = 10; ii < 101; ii += 10){
+        console.log(ii)
+
+    
+}
