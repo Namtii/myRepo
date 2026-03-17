7ac79bbe7c9c902c20da21d714a8c3953f95818a
Namtii <hit4@o2.pl>
2026-03-17 22:36:29 +0100

    push, pop, unshift, shift challenge Practice Time - Part 2 Exercise #1  https://scrimba.com/learn-javascript-c0v/~03a/s0vj66melh/head
    My progres in Learning JavaScript from scrimba.com

---
+++ push, pop, unshift, shift challenge Practice Time - Part 2 Exercise #1/index.js
@@ -0,0 +1,33 @@
+let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+
+// You need to help me fixup the largeCountries array so that 
+// China and Pakistan are added back into their respective places
+
+
+largeCountries.shift();       // usuwa "Tuvalu"
+largeCountries.pop();         // usuwa "Monaco"
+largeCountries.unshift("China");  // dodaje "China" na początek
+largeCountries.push("Pakistan");  // dodaje "Pakistan" na koniec
+
+
+
+console.log(largeCountries)
+// largeCountries.pop()
+// console.log(largeCountries)
+// largeCountries.pop()
+// console.log(largeCountries)
+
+// let india = largeCountries.shift();
+// largeCountries.shift()
+// console.log(largeCountries)
+
+// largeCountries.unshift("China","USA")
+// largeCountries.push("Indonesia","Pakistan")
+// console.log(largeCountries)
+
+
+
+
+
+// Use push() & pop() and their counterparts unshift() & shift()
+// Google how to use unshift() and shift()
