2884d3a5d8d406d139916754b2ff756a61571767
Namtii <hit4@o2.pl>
2026-03-17 22:42:53 +0100

    Logical operators Practice Time - Part 2 Exercise #1  https://scrimba.com/learn-javascript-c0v/~03b/s0n5v1np90/head
    My progres in Learning JavaScript from scrimba.com

---
+++ Logical operators Practice Time - Part 2 Exercise #1/index.js
@@ -0,0 +1,13 @@
+let dayOfMonth = 13
+let weekday = "Friday"
+
+// If it is Friday the 13th, log out this spooky face: 😱
+// Use the logical "AND operator" -> &&
+
+if(dayOfMonth === 13 && weekday === "Friday"){
+    console.log("😱")
+    
+}
+
+
+
