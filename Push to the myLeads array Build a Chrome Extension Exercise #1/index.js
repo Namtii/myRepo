36d7ea574f7df25eeb2f538339fffa166baf1528
Namtii <hit4@o2.pl>
2026-03-21 23:56:31 +0100

    Push to the myLeads array Build a Chrome Extension Exercise #1 https://scrimba.com/learn-javascript-c0v/~03r/s02mlgdi5d/head
    My progres in Learning JavaScript from scrimba.com

---
+++ Push to the myLeads array Build a Chrome Extension Exercise #1/index.js
@@ -0,0 +1,16 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+// Push the value "www.awesomelead.com" to myArray when the input button is clicked
+
+
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push("www.awesomelead.com")
+    console.log(" my array " + myLeads  )
+    
+    console.log("Button clicked!")
+})
+
+
