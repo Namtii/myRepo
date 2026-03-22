c09270c3822a90f65961be1a7150823d567597ed
Namtii <hit4@o2.pl>
2026-03-22 10:16:04 +0100

    Use a for loop to log out leads build a Chrome Extension Exercise #1 https://scrimba.com/learn-javascript-c0v/~03t/s0p96mg2is/head
    My progres in Learning JavaScript from scrimba.com

---
+++ Use a for loop to log out leads build a Chrome Extension Exercise #1/index.js
@@ -0,0 +1,17 @@
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+ 
+})
+
+    for(let i=0; i<myLeads.length; i++){
+    console.log("actual array data: " + myLeads[i])
+}
+// Log out the items in the myLeads array using a for loop 
+
+
+
