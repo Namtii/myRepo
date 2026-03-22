47739a60993329e7d60f6241ccd013a6dbc03ffa
Namtii <hit4@o2.pl>
2026-03-22 22:23:44 +0100

    More innerHTML practice Build a Chrome Extension Exercise #1 https://scrimba.com/learn-javascript-c0v/~042/s0oi1i17rt/head
    My progres in Learning JavaScript from scrimba.com

---
+++ More innerHTML practice Build a Chrome Extension Exercise #1/index.js
@@ -0,0 +1,14 @@
+
+const container = document.getElementById("container")
+
+container.innerHTML = "<button onclick='buy()'>Buy!</button>"
+
+// When clicked, render a paragraph under the button (in the container)
+// that says "Thank you for buying!"
+
+function buy(){
+ 
+    container.innerHTML =   "<button onclick='buy()'>Buy!</button>" + "<p>Thank you for buying!</p>"
+    
+    
+}
