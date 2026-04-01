7137aa74f2891737ec681eaa963cee941f8c7471
Namtii <hit4@o2.pl>
2026-04-02 00:18:17 +0200

    Arrays as parameters  https://scrimba.com/learn-javascript-c0v/~050/s006ct2cgg/head
    My progres in Learning JavaScript from scrimba.com

---
+++ Arrays as parameters/index.js
@@ -0,0 +1,8 @@
+// Create a function, getFirst(arr), that returns the first item in the array
+let arr = ["blablabla","blebleble"];
+
+let  getFirst = (arr) => arr[0];
+
+
+// Call it with an array as an argument to verify that it works
+console.log(getFirst(arr))
