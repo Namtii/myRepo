96b2a43a737445b8829b0baa1a41a510b4be1234
Namtii <hit4@o2.pl>
2026-04-08 22:45:57 +0200

    Object Destructuring Challenge Advanced Foundations https://scrimba.com/advanced-javascript-c03kpi3kss/~0xpw/s0hlc9k30c/head
    My progres in Learning JavaScript from scrimba.com

---
+++ Object Destructuring Challenge Advanced Foundations/index.js
@@ -0,0 +1,24 @@
+const dreamHoliday = {
+    destination: 'mutlivers ship',
+    activity: 'discover',
+    accommodation: 'captain_ship_room',
+    companion: 'close_ones_family'
+}
+
+const {destination, activity, accommodation, companion} = dreamHoliday
+
+console.log(`I would love to go with my ${companion} to  ${destination} to see  all posibilities and ${activity} new technology flying over multivers and sleeping inside ${accommodation}`)
+ 
+
+/* 
+Challenge
+1. Complete the object dreamHoliday with whatever
+   information is true for you. Feel free to add 
+   extra properties or change the existing ones. 
+2. Destructure the object and use the individual 
+   variables to log out one or more sentences about 
+   your dream holiday. 
+   
+E.g. "I would love to go to Austin, Texas to visit the Tesla HQ. 
+     I'd sleep in a luxury ranch and hang out with Elon Musk all day."
+*/
