0afc8b4099f7ef01190f8b2afcd924b0248f7b95
Namtii <hit4@o2.pl>
2026-04-15 19:38:45 +0200

    zadanie 4
    nauka Java script

--- zadanie4.js
+++ zadanie4.js
@@ -6,38 +6,36 @@ const arr = [1, 2, 3, 4, 5];
 
 for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 !== 0) {
-        console.log(arr[i]);
+        console.log(`Odd elements using for loop: ${arr[i]}`);
     }};
 
-console.log('Odd elements using for loop:');
+
 // TODO: your code goes here
 
 // 2. Do the same using forEach
 
-arr.forEach(zmienna => {
-    if (zmienna % 2 !== 0) {
-        console.log(zmienna);
+arr.forEach((value, index) => {
+    if (value % 2 !== 0) {
+        console.log(`Odd elements using forEach: ${arr[index]}`);
     }});
-
-
-console.log('Odd elements using forEach:');
+ 
 // TODO: your code goes here
 
 // 3. Do the same using for...of
 
 for (const num  of arr) {{
     if (num % 2 != 0){
-        console.log(num);     
+        console.log(`Odd elements using for...of: ${num}`);     
     }
 }};
-console.log('Odd elements using for...of:');
+
 // TODO: your code goes here
 
 for (const num in arr) {
     if (arr[num] % 2 !==0)
-    console.log(arr[num]);
+    console.log(`Odd elements using for...in: ${arr[num]}`);
 };
 
 // 4. Do the same using for...in
-console.log('Odd elements using for...in:');
+ 
 // TODO: your code goes here
