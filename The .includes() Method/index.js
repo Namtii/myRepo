18f2d9b0d2f84f6204dd999b1996fae6d68913c7
Namtii <hit4@o2.pl>
2026-04-18 23:48:45 +0200

    The .includes() Method https://scrimba.com/advanced-javascript-c03kpi3kss/~02g5
    My progres in Learning JavaScript from scrimba.com

---
+++ The .includes() Method/index.js
@@ -0,0 +1,40 @@
+// const emojis = ['🐥','🐯','🐼']
+// console.log(emojis.includes('🐴'))
+
+const addItemBtn = document.getElementById('add-item-btn')
+const itemInput = document.getElementById('item-input')
+const list = document.getElementById('list')
+
+const shoppingList = []
+    
+addItemBtn.addEventListener('click', function(){
+    if (shoppingList.includes(itemInput.value)) {
+        itemInput.value = ''
+        console.log("no duplicates")    
+     
+    } else {
+        shoppingList.push(itemInput.value)
+        render()        
+        itemInput.value = ''
+    }
+/*
+Challenge:
+1. Add an if else to the event listener's function.
+2. Only add an item to the shoppingList array if it 
+   is not already in the shoppingList array.
+3. If an item is a duplicate, clear the input field
+   and log out "no duplicates".
+*/
+ 
+})
+
+function render(){    
+    let html = ''
+    for (let item of shoppingList){
+        html+= `<li class="list-item">${item}</li>`
+    }
+    list.innerHTML = html
+}
+
+render()
+
